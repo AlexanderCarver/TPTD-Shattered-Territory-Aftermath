40041a9285ea38633913c7763e863c75e48cb22a
Alexander <90519515+AlexanderCarver@users.noreply.github.com>
2021-11-07 02:31:03 +0300

    Добавление стартовых аддонов.

---
+++ addons/wardrobe/lua/autorun/freebodygroupr_cmenu.lua
@@ -0,0 +1,7 @@
+﻿list.Set( "DesktopWindows", "Free Bodygroupr Icon", {
+    title = "Внешний вид",
+    icon = "icon64/tool.png",
+    init = function( icon, window )
+        RunConsoleCommand( "bodygroupwardrobe" )
+    end
+} )
---
+++ addons/wardrobe/lua/entities/free_bodygroupr/shared.lua
@@ -0,0 +1,8 @@
+﻿ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+
+ENT.PrintName = "Шкаф с обмундированием"
+ENT.Author = "SweptThrone"
+ENT.Contact = "sweptthrone971@gmail.com"
+ENT.Category = "Throneco Stuff"
+ENT.Spawnable = true
