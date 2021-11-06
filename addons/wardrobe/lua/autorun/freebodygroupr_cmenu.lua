602825ca2c99f40d31dd94d76a0c2e8a0ffdd104
Alexander <90519515+AlexanderCarver@users.noreply.github.com>
2021-11-07 02:31:56 +0300

    Фикс фикса фикса аддонов.

--- addons/wardrobe/lua/autorun/freebodygroupr_cmenu.lua
+++
@@ -1,7 +0,0 @@
-﻿list.Set( "DesktopWindows", "Free Bodygroupr Icon", {
-    title = "Внешний вид",
-    icon = "icon64/tool.png",
-    init = function( icon, window )
-        RunConsoleCommand( "bodygroupwardrobe" )
-    end
-} )
