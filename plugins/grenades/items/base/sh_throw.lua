6c7331038fa184ac0b1ea36f2744ed6d7541fe2c
Alexander <90519515+AlexanderCarver@users.noreply.github.com>
2021-11-14 17:48:54 +0300

    Добавление флаера.

---
+++ plugins/grenades/items/base/sh_throw.lua
@@ -0,0 +1,26 @@
+ITEM.name = "Throwable Object"
+ITEM.model = "models/Items/grenadeAmmo.mdl"
+ITEM.description = "Throwable Object Example"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.throw = "ix_flare"
+ITEM.throwforce = 2500
+
+-- You can use hunger table? i guess? 
+ITEM.functions = ITEM.functions or {}
+ITEM.functions.throw = {
+	name = "Активировать",
+	icon = "icon16/arrow_up.png",
+	OnRun = function(item)
+		local client = item.player
+		local grd = ents.Create( item.throw )
+		grd:SetPos( client:EyePos() + client:GetAimVector() * 50 )
+		grd:Spawn()
+
+		local phys = grd:GetPhysicsObject()
+		phys:SetVelocity( client:GetAimVector() * item.throwforce * math.Rand( .8, 1 ) )
+		phys:AddAngleVelocity( client:GetAimVector() * item.throwforce  )
+		
+		return true
+	end,
+}
