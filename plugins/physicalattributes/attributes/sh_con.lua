3c2957784f32fc0daba89f2742333d163e959103
Alexander <90519515+AlexanderCarver@users.noreply.github.com>
2021-11-14 19:35:59 +0300

    Новая система аттрибутов.

---
+++ plugins/physicalattributes/attributes/sh_con.lua
@@ -0,0 +1,6 @@
+﻿ATTRIBUTE.name = "Конституция"
+ATTRIBUTE.description = ""
+
+function ATTRIBUTE:OnSetup(client, value)
+	client:SetMaxHealth(ix.config.Get("defaultMaxHealth") + (value * ix.config.Get("constitutionMultiplier")))
+end
