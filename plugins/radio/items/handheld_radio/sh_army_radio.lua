f22582d375bea8dbda73bd9ff97ef0241b86fc59
Alexander <90519515+AlexanderCarver@users.noreply.github.com>
2021-11-15 00:08:11 +0300

    Радио.

--- plugins/radio/items/handheld_radio/sh_army_radio.lua
+++ plugins/radio/items/handheld_radio/sh_army_radio.lua
@@ -1,4 +1,6 @@
 ﻿ITEM.name = "Рация AN/PRC-152A"
 ITEM.description = "Небольшая, легкая и прочная переносимая радиостанция ВС США, которая оснащает пользователя одновременной узкополосной и широкополосной связью, а также тактической связью командования и управления в различных сложных условиях."
-ITEM.model = "models/lt_c/sci_fi/holo_tablet.mdl"
-ITEM.category = "Приборы"
+ITEM.model = "models/illusion/eftcontainers/gasanalyser.mdl"
+ITEM.height = 1
+ITEM.width = 1
+ITEM.weight = 0.430
--- plugins/radio/items/handheld_radio/sh_corporate_radio.lua
+++ plugins/radio/items/handheld_radio/sh_corporate_radio.lua
@@ -1,4 +1,6 @@
 ﻿ITEM.name = "Передатчик связи HDCT-25"
 ITEM.description = "Странного вида передатчик чёрного цвета из, как Вам кажется, пластика, который внешне чем-то напоминает рацию и планшет в одном формате. Во время того, как Вы берёте его в руки, его экран активируется и показывает логотип корпорации Tower Corporation."
-ITEM.model = "models/lt_c/sci_fi/holo_tablet.mdl"
-ITEM.category = "Приборы"
+ITEM.model = "models/illusion/eftcontainers/lcdclean.mdl"
+ITEM.height = 1
+ITEM.width = 1
+ITEM.weight = 0.380
