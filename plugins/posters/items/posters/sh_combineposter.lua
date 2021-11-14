49cdfe5aa2e7c38877769cdc743c8ef192db4344
Alexander <90519515+AlexanderCarver@users.noreply.github.com>
2021-11-14 21:39:30 +0300

    Удаление лишнего говна.

--- plugins/posters/items/posters/sh_combineposter.lua
+++
@@ -1,9 +0,0 @@
-ITEM.name = "Breen Poster"
-ITEM.description = "A poster of Earth Administrator Dr.Breen."
-ITEM.category = "Poster"
-ITEM.model = "models/props_junk/garbage_newspaper001a.mdl"
-ITEM.poster = "posterbreen"
-ITEM.width = 1
-ITEM.height = 1
-ITEM.weight = 1.5
-ITEM.maxStacks = 5
