29903d9f78d698c4e04cf5dfb131b9e38f7f23f4
Alexander <90519515+AlexanderCarver@users.noreply.github.com>
2022-07-06 20:40:43 +0300

    Вырезал старые карты

--- stalkerrp/plugins/cards/items/sh_playingcards.lua
+++
@@ -1,5 +0,0 @@
-ITEM.name = "Маленькая колода карт"
-ITEM.description = "Колода игральных карт, в которой отсутствуют два джокера."
-ITEM.model = "models/props_lab/box01a.mdl"
-ITEM.price = 15
-ITEM.skin = 1
