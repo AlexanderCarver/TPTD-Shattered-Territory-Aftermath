82248d49ac31fa95f765c0af23fc065b5f23ae2a
Dix <90519515+AlexanderCarver@users.noreply.github.com>
2022-07-14 22:46:05 +0300

    Update sv_schema.lua

--- stalkerrp/schema/sv_schema.lua
+++ stalkerrp/schema/sv_schema.lua
@@ -3,7 +3,7 @@
 ------------------------------------------------------------------------
 
 -- Workshop Items ( 2290918731 ) --
-
+/*
 resource.AddWorkshop( '2375197722' ) -- [TPTD] Props Pack (Defense and Barricades)
 resource.AddWorkshop( '2375191747' ) -- [TPTD] Props Pack (Food and Liquids)
 resource.AddWorkshop( '2375024834' ) -- [TPTD] Props Pack (Medicine and Drugs)
@@ -11,3 +11,4 @@ resource.AddWorkshop( '2375159796' ) -- [TPTD] Props Pack (Interior and Exterior
 resource.AddWorkshop( '2375216244' ) -- [TPTD] Props Pack (Other and Misc)
 resource.AddWorkshop( '2428613977' ) -- [TPTD] Props Pack (Crafting and Components)
 resource.AddWorkshop( '2428568315' ) -- [TPTD] Props Pack (Equipment and Personal Items)
+*/
