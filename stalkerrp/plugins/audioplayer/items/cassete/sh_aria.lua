1db5b514495297f20499609cd58739c660f7316d
Alexander <90519515+AlexanderCarver@users.noreply.github.com>
2022-12-26 00:32:25 +0300

    Удалил лишние плагины.

--- stalkerrp/plugins/audioplayer/items/cassete/sh_aria.lua
+++
@@ -1,6 +0,0 @@
-ITEM.name = "Ария-Беспечный Ангел нет"
-
-ITEM.musicID = "aria"
-ITEM.volume = 0.8
-ITEM.music = "tf2_radio/31_aria.mp3"
-ITEM.category = "Кассеты"
--- stalkerrp/plugins/campfire/entities/entities/ix_campfire/shared.lua
+++
@@ -1,8 +0,0 @@
-﻿DEFINE_BASECLASS("base_gmodentity");
-
-ENT.Type = "anim";
-ENT.Author = "TPTD";
-ENT.PrintName = "Campfire";
-ENT.Category = "TPTD"
-ENT.Spawnable = true;
-ENT.AdminSpawnable = true;
