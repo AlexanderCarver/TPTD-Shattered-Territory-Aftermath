4afb8f2ab9990c1188bebce54aafe8daa86ee882
Alexander <90519515+AlexanderCarver@users.noreply.github.com>
2023-04-08 02:05:23 +0300

    Antrax

---
+++ headshot_effects/sh_plugin.lua
@@ -0,0 +1,5 @@
+PLUGIN.name = "Система контузии"
+PLUGIN.author = "ChatGPT"
+PLUGIN.description = "Добавляет эффект контузии при получении урона по голове."
+
+ix.util.Include("sv_hooks.lua", "server")
