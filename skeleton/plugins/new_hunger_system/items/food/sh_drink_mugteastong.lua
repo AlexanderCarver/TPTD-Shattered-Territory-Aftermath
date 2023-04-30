53e98dfc7fd5b06d8b44294070c68296ca8b2922
Alexander <90519515+AlexanderCarver@users.noreply.github.com>
2023-04-30 22:24:11 +0300

    Система еды и консервов

---
+++ skeleton/plugins/new_hunger_system/items/food/sh_drink_mugteastong.lua
@@ -0,0 +1,31 @@
+ITEM.name = "Кружка чифиря"
+ITEM.description = "Маленькая железная кружка, подаваемая в баре."
+ITEM.longdesc = "Напиток, получаемый вывариванием высококонцентрированной заварки чая. Пользуется большим спросом среди заключённых пенитенциарных учреждений... Обладает психостимулирующим действием, слегка расслабляет."
+ITEM.model = "models/lostsignalproject/items/medical/drink_crow.mdl"
+
+ITEM.price = 105
+ITEM.height = 1
+ITEM.width = 1
+ITEM.weight = 0.095
+ITEM.flatweight = 0.030
+
+ITEM.isDrink = true
+
+--ITEM.hunger = 15
+ITEM.thirst = -12
+ITEM.quantity = 1
+--ITEM.alcohol = 10
+
+ITEM.sound = "stalkersound/inv_flask.mp3" --cannedfoodeat.mp3
+
+function ITEM:PopulateTooltipIndividual(tooltip)
+    ix.util.PropertyDesc(tooltip, "Стимулирует", Color(64, 224, 208))
+	ix.util.PropertyDesc(tooltip, "Вызывает жажду", Color(64, 224, 208))
+end
+
+ITEM:Hook("use", function(item)
+	item.player:EmitSound(item.sound or "items/battery_pickup.wav")
+	ix.chat.Send(item.player, "iteminternal", "берет кружку с чаем и выпивает все за раз.", false)
+end)
+
+ITEM:DecideFunction()
---
+++ skeleton/plugins/new_hunger_system/items/food/sh_sux_mre.lua
@@ -0,0 +1,29 @@
+ITEM.name = "ИРП ВС США"
+ITEM.description = "Сухой паёк MRE, сделанный для нужд армии США."
+ITEM.longdesc = "MRE (Meal, Ready-to-Eat) (Обед, Готовый-к-Употреблению) являются аналогом наших отечественных сухпайков, разработанных для армии США. Упаковка MRE рассчитана на суровые условия эксплуатации и защиту от воздействия стихии, что позволяет особо не заботиться об условиях хранения. Содержит в себе массу различных продуктов для утоления голода в полевых условиях."
+ITEM.model = "models/illusion/eftcontainers/mre.mdl"
+
+ITEM.price = 1200
+ITEM.height = 2
+ITEM.width = 1
+ITEM.weight = 0.150
+ITEM.flatweight = 0.060
+
+ITEM.hunger = 22
+ITEM.thirst = 10
+ITEM.quantity = 8
+--ITEM.alcohol = 10
+
+ITEM.sound = "stalkersound/inv_flask.mp3" --cannedfoodeat.mp3
+
+function ITEM:PopulateTooltipIndividual(tooltip)
+    ix.util.PropertyDesc(tooltip, "Консервированная пища", Color(64, 224, 208))
+	ix.util.PropertyDesc(tooltip, "Хорошо утоляет голод", Color(64, 224, 208))
+end
+
+ITEM:Hook("use", function(item)
+	item.player:EmitSound(item.sound or "items/battery_pickup.wav")
+	ix.chat.Send(item.player, "iteminternal", "аккуратно вскрывает консервированную банку и употребляет находящийся внутри продукт. Фасоль в томатном соусе достаточно вкусная, чтобы утолить аппетит, однако ее лучше всего употреблять вместе с макаронами.", false)
+end)
+
+ITEM:DecideFunction()
