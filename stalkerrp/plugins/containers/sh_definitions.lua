571d2afa712ef41d7e609dc474bbb52c1f28b42c
Rabotaet3 <RabotaetYak@gmail.com>
2023-06-01 18:12:30 +0300

    Различные предметы(тайнички)

--- stalkerrp/plugins/containers/sh_definitions.lua
+++ stalkerrp/plugins/containers/sh_definitions.lua
@@ -9,118 +9,104 @@
 	})
 ]]--
 
-ix.container.Register("models/props_junk/wood_crate001a.mdl", {
-	name = "Crate",
-	description = "A simple wooden crate.",
-	width = 4,
-	height = 4,
-})
+    ix.container.Register("models/lostsignalproject/items/quest/large_case.mdl", {
+        name = "Оружейный ящик",
+        description = "Старый, ржавый оружейный ящик. Места не много, но это лучше, чем ничего.",
+        width = 5,
+        height = 3,
+        locksound = "",
+        opensound = ""
+    })
 
-ix.container.Register("models/props_c17/lockers001a.mdl", {
-	name = "Locker",
-	description = "A white locker.",
-	width = 3,
-	height = 5,
-})
+    ix.container.Register("models/lostsignalproject/items/misc/backpack.mdl", {
+        name = "Грязный рюкзак",
+        description = "Старый рюкзак. Вряд ли получится его использовать по назначению, но для хранения своих сокровищ - вполне.",
+        width = 4,
+        height = 4,
+        locksound = "",
+        opensound = ""
+    })
 
-ix.container.Register("models/props_wasteland/controlroom_storagecloset001a.mdl", {
-	name = "Metal Cabinet",
-	description = "A green metal cabinet.",
-	width = 4,
-	height = 5,
-})
+	    ix.container.Register("models/props/de_prodigy/prodcratesb.mdl", {
+        name = "Большой деревянный ящик",
+        description = "Старый деревянный ящик. В некоторых местах живёт плесень, но для хранения огромного количества вещей этого будет достаточно.",
+        width = 12,
+        height = 8,
+        locksound = "",
+        opensound = ""
+    })
 
-ix.container.Register("models/props_wasteland/controlroom_storagecloset001b.mdl", {
-	name = "Metal Cabinet",
-	description = "A green metal cabinet.",
-	width = 4,
-	height = 5,
-})
-
-ix.container.Register("models/props_wasteland/controlroom_filecabinet001a.mdl", {
-	name = "File Cabinet",
-	description = "A metal filing cabinet.",
-	width = 5,
-	height = 3
-})
-
-ix.container.Register("models/props_wasteland/controlroom_filecabinet002a.mdl", {
-	name = "File Cabinet",
-	description = "A metal filing cabinet.",
-	width = 3,
-	height = 6,
-})
-
-ix.container.Register("models/props_lab/filecabinet02.mdl", {
-	name = "File Cabinet",
-	description = "A metal filing cabinet.",
-	width = 5,
-	height = 3
-})
-
-ix.container.Register("models/props_c17/furniturefridge001a.mdl", {
-	name = "Refrigerator",
-	description = "A metal box for keeping food in.",
-	width = 2,
-	height = 3,
-})
-
-ix.container.Register("models/props_wasteland/kitchen_fridge001a.mdl", {
-	name = "Large Refrigerator",
-	description = "A large metal box for storing even more food in.",
-	width = 4,
-	height = 5,
-})
-
-ix.container.Register("models/props_junk/trashbin01a.mdl", {
-	name = "Trash Bin",
-	description = "What do you expect to find in here?",
-	width = 2,
-	height = 2,
-})
-
-ix.container.Register("models/props_junk/trashdumpster01a.mdl", {
-	name = "Dumpster",
-	description = "A dumpster meant to stow away trash. It emanates an unpleasant smell.",
-	width = 6,
-	height = 3
-})
-
-ix.container.Register("models/items/ammocrate_smg1.mdl", {
-	name = "Ammo Crate",
-	description = "A heavy crate that stores ammo.",
-	width = 5,
-	height = 3,
-	OnOpen = function(entity, activator)
-		local closeSeq = entity:LookupSequence("Close")
-		entity:ResetSequence(closeSeq)
-
-		timer.Simple(2, function()
-			if (entity and IsValid(entity)) then
-				local openSeq = entity:LookupSequence("Open")
-				entity:ResetSequence(openSeq)
-			end
-		end)
-	end
-})
-
-ix.container.Register("models/props_forest/footlocker01_closed.mdl", {
-	name = "Footlocker",
-	description = "A small chest to store belongings in.",
-	width = 5,
-	height = 3
-})
-
-ix.container.Register("models/Items/item_item_crate.mdl", {
-	name = "Item Crate",
-	description = "A crate to store some belongings in.",
-	width = 5,
-	height = 3
-})
-
-ix.container.Register("models/props_c17/cashregister01a.mdl", {
-	name = "Cash Register",
-	description = "A register with some buttons and a drawer.",
-	width = 2,
-	height = 1
-})
+	ix.container.Register("models/lostsignalproject/items/repair/exo_repair_kit.mdl", {
+        name = "Синий защитный контейнер",
+        description = "Старый металлический контейнер. Выглядит потрёпанным и забытым. Ящик изначально был кем-то открыт. На боковой стороне странная маркировка: НИИ ОПрМШ.",
+        width = 5,
+        height = 5,
+        locksound = "",
+        opensound = ""
+    })
+	
+	ix.container.Register("models/lostsignalproject/items/misc/kithunt.mdl", {
+        name = "Маленькая сумка",
+        description = "Маленькая сумочка, в которой можно хранить небольшие предметы.",
+        width = 3,
+        height = 2,
+        locksound = "",
+        opensound = ""
+    })
+	
+	ix.container.Register("models/lostsignalproject/items/repair/toolkit_wp.mdl", {
+        name = "Старый ржавый инструментный ящик",
+        description = "Старый ржавый ящик, который раньше, скорее всего, использовали для инструментов.",
+        width = 4,
+        height = 1,
+        locksound = "",
+        opensound = ""
+    })
+	
+	ix.container.Register("models/mosi/fnv/props/workstations/campfire05.mdl", {
+        name = "Старое место для костра",
+        description = "Делали наскоро. Угли давно сгорели, а на дне кострюли осталась сгнившая, давно покрывшаяся плесенью еда. Можно использовать для временных тайников.",
+        width = 2,
+        height = 2,
+        locksound = "",
+        opensound = ""
+    })
+	ix.container.Register("models/lostsignalproject/items/misc/box_paper.mdl", {
+        name = "Старая коробка",
+        description = "Коробка пропитана влагой, от чего в некоторых местах проросла плесень. На коробке имеется маленькая записка, адресованная Ярику: Ярик, они нас раскрылы.. Мы проебались.",
+        width = 3,
+        height = 2,
+        locksound = "",
+        opensound = ""
+    })
+	ix.container.Register("models/mosi/fnv/props/workstations/workbench.mdl", {
+        name = "Старый столик с инструментами.",
+        description = " Выглядит хорошо сохранившимся, несмотря на тяжёлые условия Сибири.",
+        width = 4,
+        height = 2,
+        locksound = "",
+        opensound = ""
+    })
+	ix.container.Register("models/props/CS_militia/refrigerator01.mdl", {
+        name = "Ржавый сгнивший шкафчик",
+        description = "Шкафчик, который пережил многое. Время и сырость не жалеет никого.",
+        width = 3,
+        height = 6,
+        locksound = "",
+        opensound = ""
+    })
+	ix.container.Register("models/lostsignalproject/items/misc/box_metal.mdl", {
+        name = "Ящик для различных нужд",
+        description = "Маленький, потрёпанный, но использовать можно.",
+        width = 3,
+        height = 3,
+        locksound = "",
+        opensound = ""
+    })
+	ix.container.Register("models/mosi/fnv/props/workstations/workbench.mdl", {
+        name = "Деревяный ящик",
+        description = "Обычный деревянный ящик. Не маленький, но и не большой.",
+        width = 4,
+        height = 4,
+        locksound = "",
+        opensound = ""
